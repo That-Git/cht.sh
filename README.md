26c440001f911479c9f750159896ce600c4d3825
R <jugle.orgx99@gmail.com>
2024-05-09 16:21:20 +0100

    add files

---
+++ README.md
@@ -0,0 +1,2 @@
+# [I aware of the rust curl crate](https://crates.io/crates/curl)
+however I had some issues with it so here we are
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::{io::Write, process::{Command, Stdio}};
+
+fn prompt(prompt:&str) -> String {
+    let mut line = String::new();
+    print!("{}", prompt);
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut line).expect("Error: Could not read a line");
+
+    return line.trim().to_string()
+}
+
+fn main() /* -> &'static str */ {
+    let topic: String = prompt("topic: ");
+    let query: String = prompt("query: ");
+    // println!("{}", (format!("https://cht.sh/{}/{}", topic.to_string(), query.to_string())).as_str());
+    let output = Command::new("curl").arg((format!("https://cht.sh/{}/{}", topic.to_string(), query.to_string())).as_str())
+        // Tell the OS to record the command's output
+        .stdout(Stdio::piped())
+        // execute the command, wait for it to complete, then capture the output
+        .output()
+        // Blow up if the OS was unable to start the program
+        .unwrap();
+    // extract the raw bytes that we captured and interpret them as a string
+    let hypertext = String::from_utf8(output.stdout).unwrap();
+    println!("{}", hypertext);
+}
